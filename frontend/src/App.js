36f4e31c521509509f3d81f6191a7cb2a4d77341
SewonKim0 <sewonkim0black@gmail.com>
2025-04-21 22:04:15 -0400

    Frontend: created output UI + added font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,41 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [outputs, setOutputs] = useState([]);
+
+  useEffect(() => {
+    // TEST
+    setOutputs([
+      "Hello, world!",
+      "This is a test message.",
+      "How are you doing today?",
+    ]);
+
+    // TEST
+    setTimeout(() => {
+      setOutputs((prevOutputs) => [
+        ...prevOutputs,
+        "This is a new message.",
+      ]);
+    }, 2000);
+  }, []);
+
   return (
     <div className="home">
+      {/* Header */}
+      <div className="header">
+        <h1> ChatNYU </h1>
+      </div>
+
+      {/* Output */}
+      <div className="output">
+        {outputs.map((output, index) => (
+          <div key={index} className="output-item">
+            <p>{output}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
